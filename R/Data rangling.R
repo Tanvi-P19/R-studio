2d3452d1aece3ae93bcbc434fc9f4f4f1758cc4b
Tanvi-P19 <tanvip1906@gmail.com>
2026-07-29 15:31:33 +0530

    Initial commit

--- R/Data rangling.R
+++ R/Data rangling.R
@@ -1,8 +1,25 @@
 #Data Wrangling
 library(usethis)
+use_git()
 use_github()
 library(nycflights13)
 library(tidyverse)
 install.packages("nycflights13")
 flights
 glimpse(flights)
+flights |> 
+  filter(dep_delay > 120)
+# Flights that departed on January 1
+flights |> 
+  filter(month == 1 & day == 1)
+# Flights that departed in January or February
+flights |> 
+  filter(month == 1 | month == 2)
+# A shorter way to select flights that departed in January or February
+flights |> 
+  filter(month %in% c(1, 2))
+
+flights |> 
+  filter(month == 2 & day == 1)
+flights |> 
+  filter(month == 1 & month == 2)
